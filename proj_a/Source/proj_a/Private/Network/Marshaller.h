539ab04ae7549c0e3d8f412f7fbd80e99f8187ac
Hansangmin <ghtrm1037@gmail.com>
2024-04-17 23:21:52 +0900

    FEAT: 일반 데이터 및 protobuf serialize, deserialize 구현

---
+++ proj_a/Source/proj_a/Private/Network/Marshaller.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Serialization/MemoryWriter.h"
+#include "Serialization/BufferArchive.h"
+// #include "protoGenerated/playerData.pb.h"
+
+/**
+ * 
+ */
+
+class Marshaller
+{
+public:
+	template <typename T>
+	static TArray<uint8> serializeData(const T& data)
+	{
+		FBufferArchive archive;
+
+		for (auto d : data)
+			archive << d;
+		
+		TArray<uint8> outData;
+		outData.Append(archive.GetData(), archive.Num());
+		archive.FlushCache();
+		archive.Empty();
+
+		return outData;
+	}
+
+	template <typename T>
+	static T deserializeData(const TArray<uint8>& inData)
+	{
+		T data;
+		FMemoryReader Reader(inData);
+
+		for (auto d : data)
+			Reader << d;
+
+		Reader.FlushCache();
+		Reader.Close();
+
+		return data;
+	}
+
+	template <typename T>
+	static TArray<uint8> protoSerializeData(const T& data)
+	{
+		std::string buffer;
+		data.SerializeToString(&buffer);
+		
+		TArray<uint8> OutData;
+		OutData.Append(reinterpret_cast<const uint8*>(buffer.data()), buffer.size());
+    
+		return OutData;
+	}
+
+	template <typename T>
+	static bool protoDeserializeData(const TArray<uint8>& inData, T& outData)
+	{
+		return outData.ParseFromArray(inData.GetData(), inData.Num());
+	}
+};
---
+++ proj_a/Source/proj_a/Private/Network/NetWorkDataProcesser.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "NetWorkDataProcesser.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_A_API UNetWorkDataProcesser : public UObject
+{
+	GENERATED_BODY()
+};
