f7eb314bdbd3dcd098c6c07584638216e4ef46e4
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2024-05-11 22:34:53 +0900

    FEAT: animation retarget character, add ui c++

---
+++ proj_a/Source/proj_a/Widget/Public/DPLoginWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DPLoginWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_A_API UDPLoginWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
