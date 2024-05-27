b5e4cd33d09be4a41a4f8dc002af91c436f55b04
deulee <ideade9@gmail.com>
2024-05-27 13:51:56 +0900

    MOVE: DPGameMode Moved

---
+++ proj_a/Source/proj_a/GameMode/Private/DPGameModeBase.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DPGameModeBase.h"
+#include "DPCharacter.h"
+#include "DPPlayerController.h"
+#include "SocketManager.h"
+
+ADPGameModeBase::ADPGameModeBase()
+{
+	DefaultPawnClass = ADPCharacter::StaticClass();
+    //static ConstructorHelpers::FClassFinder<APawn> CHARACTER
+    //(TEXT("/Game/blueprints/bp_character.bp_character_C"));
+    //if (CHARACTER.Class != nullptr)
+    //{
+    //    DefaultPawnClass = CHARACTER.Class;
+    //}
+
+	PlayerControllerClass = ADPPlayerController::StaticClass();
+}
+
+void ADPGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	// 재시도 로직 추가 해야함.
+	FSocketManager &SocketManager = FSocketManager::GetInstance();
+	
+	bool success = SocketManager.Connect("10.19.225.124", 4242);
+	if (success) {
+		UE_LOG(LogTemp, Warning, TEXT("Connection established successfully."));
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("Failed to establish connection."));
+	}
+	FSocketManager::GetInstance().RunTask();
+}
+
+void ADPGameModeBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	UE_LOG(LogTemp, Warning, TEXT("Call end play."));
+	FSocketManager::GetInstance().Close();
+}
---
+++ proj_a/Source/proj_a/GameMode/Public/DPGameModeBase.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_A_API ADPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ADPGameModeBase();
+	virtual void StartPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+};
