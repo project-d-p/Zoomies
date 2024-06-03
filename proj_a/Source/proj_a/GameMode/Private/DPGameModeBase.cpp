239c291f8acff155ac1435232356b2438ed61612
parkjoungwan <whddhs6645@gmail.com>
2024-06-03 20:09:21 +0900

    build.cs: switch os

--- proj_a/Source/proj_a/GameMode/Private/DPGameModeBase.cpp
+++ proj_a/Source/proj_a/GameMode/Private/DPGameModeBase.cpp
@@ -19,21 +19,21 @@ void ADPGameModeBase::PostLogin(APlayerController* newPlayer)
 {
 	Super::PostLogin(newPlayer);
 
-	UE_LOG(LogTemp, Log, TEXT("mainLevel PostLogin"));
-	UE_LOG(LogTemp, Log, TEXT("SESSION JOINED: %d"), GetNumPlayers());
-	TArray<AActor*> FoundCharacters;
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ADPCharacter::StaticClass(), FoundCharacters);
-	int32 NumberOfCharacters = FoundCharacters.Num();
-
-	UE_LOG(LogTemp, Log, TEXT("Number of ADPCharacters in the world: %d"), NumberOfCharacters);
-	ADPCharacter* player_character = Cast<ADPCharacter>(newPlayer->GetPawn());
-	if (player_character != nullptr)
-	{
-		// if i set this to false, the character in client side was not seen.
-		// player_character->SetReplicates(false);
-		// if i set this to false, i don't know what happen. Nothings Different.
-		player_character->GetCharacterMovement()->SetIsReplicated(false);
-	}
+	// UE_LOG(LogTemp, Log, TEXT("mainLevel PostLogin"));
+	// UE_LOG(LogTemp, Log, TEXT("SESSION JOINED: %d"), GetNumPlayers());
+	// TArray<AActor*> FoundCharacters;
+	// UGameplayStatics::GetAllActorsOfClass(GetWorld(), ADPCharacter::StaticClass(), FoundCharacters);
+	// int32 NumberOfCharacters = FoundCharacters.Num();
+	//
+	// UE_LOG(LogTemp, Log, TEXT("Number of ADPCharacters in the world: %d"), NumberOfCharacters);
+	// ADPCharacter* player_character = Cast<ADPCharacter>(newPlayer->GetPawn());
+	// if (player_character != nullptr)
+	// {
+	// 	// if i set this to false, the character in client side was not seen.
+	// 	// player_character->SetReplicates(false);
+	// 	// if i set this to false, i don't know what happen. Nothings Different.
+	// 	player_character->GetCharacterMovement()->SetIsReplicated(false);
+	// }
 }
 
 void ADPGameModeBase::DisableReplicationForCharacters()
--- proj_a/Source/proj_a/proj_a.Build.cs
+++ proj_a/Source/proj_a/proj_a.Build.cs
@@ -9,23 +9,31 @@ public class proj_a : ModuleRules
 {
 	public proj_a(ReadOnlyTargetRules Target) : base(Target)
 	{
-		// XXX: 배포시에 SetProtobuf() 메소드 내부로 이동.
-		string protobufPath = System.Environment.GetEnvironmentVariable("Protobuf");
+		string projectDir = Path.Combine(ModuleDirectory, "../../");
+		string thirdPartyDir = Path.Combine(projectDir, "ThirdParty");
 		
-		if (string.IsNullOrEmpty(protobufPath))
+		if (string.IsNullOrEmpty(thirdPartyDir))
 		{
-			throw new Exception("PROTOBUF_PATH environment variable is not set");
+			throw new Exception("No Protobuf Error" + thirdPartyDir + ".");
 		}
 		
-		SetProtobuf(protobufPath);
+		SetProtobuf(thirdPartyDir);
 		
 		// XXX: 배포시에 컴파일 코드 삭제(혹은 주석 처리)
-		string protocPath = Path.Combine(protobufPath, "tools", "protobuf", "protoc.exe");
-		string protoFilesPath = Path.Combine(ModuleDirectory, "Protobuf", "Proto_file");
-		string generatedProtoFilesPath = Path.Combine(ModuleDirectory, "Protobuf", "Pb_File");
-		
-		System.Console.WriteLine("Compiling .proto files...");
-		CompileProtoFiles(protocPath, protoFilesPath, generatedProtoFilesPath);
+		// string protocPath = "";
+		// if (Target.Platform == UnrealTargetPlatform.Win64)
+		// {
+		// 	protocPath = Path.Combine(protobufPath, "bin", "protoc.exe");
+		// }
+		// else if (Target.Platform == UnrealTargetPlatform.Mac)
+		// {
+		// 	protocPath = Path.Combine(protobufPath, "bin", "protoc");
+		// }
+		// string protoFilesPath = Path.Combine(ModuleDirectory, "Protobuf", "Proto_file");
+		// string generatedProtoFilesPath = Path.Combine(ModuleDirectory, "Protobuf", "Pb_File");
+		//
+		// System.Console.WriteLine("Compiling .proto files...");
+		// CompileProtoFiles(protocPath, protoFilesPath, generatedProtoFilesPath);
 		//
 		
 		PublicIncludePaths.AddRange(new string[] {
@@ -51,23 +59,26 @@ public class proj_a : ModuleRules
 		
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		string ProjectDir = Path.GetFullPath(Path.Combine(ModuleDirectory, "../../"));
-	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PublicDependencyModuleNames.AddRange(new string[]
 			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Sockets", "Networking", "OnlineSubsystemSteam", "OnlineSubsystem", "OnlineSubsystemUtils" });
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		string SteamVersion = "Steamv153";
-		string SteamSDKPath = Path.Combine("C:", "Users", "idead", "UnrealEngine", "Engine", "Source", "ThirdParty", "Steamworks", SteamVersion, "sdk");
-
+		string SteamSDKPath = Path.Combine(ModuleDirectory, "Steam");
 		if (Directory.Exists(SteamSDKPath))
 		{
 			PublicIncludePaths.Add(Path.Combine(SteamSDKPath, "public"));
-			PublicAdditionalLibraries.Add(Path.Combine(SteamSDKPath, "redistributable_bin", "win64", "steam_api64.lib"));
-			RuntimeDependencies.Add("$(ProjectDir)/Binaries/Win64/steam_api64.dll", Path.Combine(SteamSDKPath, "redistributable_bin", "win64", "steam_api64.dll"));
-			RuntimeDependencies.Add("$(ProjectDir)/steam_api64.dll", Path.Combine(SteamSDKPath, "redistributable_bin", "win64", "steam_api64.dll"));
+			if (Target.Platform == UnrealTargetPlatform.Win64)
+			{
+				PublicAdditionalLibraries.Add(Path.Combine(SteamSDKPath, "redistributable_bin", "win64", "steam_api64.lib"));
+				RuntimeDependencies.Add("$(ProjectDir)/Binaries/Win64/steam_api64.dll", Path.Combine(SteamSDKPath, "redistributable_bin", "win64", "steam_api64.dll"));
+			}
+			else if (Target.Platform == UnrealTargetPlatform.Mac)
+			{
+				PublicAdditionalLibraries.Add(Path.Combine(SteamSDKPath, "redistributable_bin", "osx", "libsteam_api.dylib"));
+				RuntimeDependencies.Add("$(ProjectDir)/Binaries/Mac/libsteam_api.dylib", Path.Combine(SteamSDKPath,"redistributable_bin", "osx", "libsteam_api.dylib"));
+			}
 		}
 		//System.Console.WriteLine($"SteamSDKPath: {SteamSDKPath}");
 		//System.Console.WriteLine("Steam SDK path exists");
@@ -83,36 +94,68 @@ public class proj_a : ModuleRules
         // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
     }
 
-	private void SetProtobuf(string protobufPath)
+	private void SetProtobuf(string thirdPartyDir)
 	{
+		string protobufPath = Path.Combine(thirdPartyDir, "Protobuf");
+		string abseilPath = Path.Combine(thirdPartyDir, "abseil-cpp");
 		PublicIncludePaths.Add(Path.Combine(protobufPath, "include"));
-		PublicAdditionalLibraries.Add(Path.Combine(protobufPath, "lib", "libprotoc.lib"));
-		PublicAdditionalLibraries.Add(Path.Combine(protobufPath, "lib", "libprotobuf-lite.lib"));
-		PublicAdditionalLibraries.Add(Path.Combine(protobufPath, "lib", "libprotobuf.lib"));
+		PublicIncludePaths.Add(Path.Combine(abseilPath, "include"));
+		
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			// Abseil
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Win64", "absl_base.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Win64", "absl_city.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Win64", "absl_hash.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Win64", "absl_low_level_hash.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Win64", "absl_raw_hash.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Win64", "absl_raw_logging_internal.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Win64", "absl_throw_delegate.lib"));
+			
+			// ProtoBuf
+			PublicAdditionalLibraries.Add(Path.Combine(protobufPath, "lib", "Win64", "libprotoc.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(protobufPath, "lib", "Win64", "libprotobuf.lib"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			// Abseil
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Mac", "libabsl_base.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Mac", "libabsl_city.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Mac", "libabsl_hash.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Mac", "libabsl_low_level_hash.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Mac", "libabsl_raw_hash.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Mac", "libabsl_raw_logging_internal.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(abseilPath, "lib", "Mac", "libabsl_throw_delegate.a"));
+
+			// ProtoBuf
+			PublicAdditionalLibraries.Add(Path.Combine(protobufPath, "lib", "Mac", "libprotoc.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(protobufPath, "lib", "Mac", "libprotobuf.a"));
+		}
 		PublicDefinitions.Add("GOOGLE_PROTOBUF_INTERNAL_DONATE_STEAL_INLINE");
+		PublicDefinitions.Add("PROTOBUF_ENABLE_DEBUG_LOGGING_MAY_LEAK_PII=0");
 	}
 	
-	private void CompileProtoFiles(string protocPath, string inputPath, string outputPath)
-	{
-		var protoFiles = Directory.GetFiles(inputPath, "*.proto");
-		foreach (var protoFile in protoFiles)
-		{
-			var process = new Process();
-			process.StartInfo.FileName = protocPath;
-			process.StartInfo.Arguments = $"--proto_path={inputPath} --cpp_out={outputPath} {protoFile}";
-			process.StartInfo.UseShellExecute = false;
-			process.StartInfo.RedirectStandardOutput = true;
-			process.StartInfo.RedirectStandardError = true;
-			process.Start();
-			process.WaitForExit();
-
-			string output = process.StandardOutput.ReadToEnd();
-			string errors = process.StandardError.ReadToEnd();
-			if (!string.IsNullOrEmpty(errors) || process.ExitCode != 0)
-			{
-				throw new Exception("Error compiling " + protoFile + ": " + errors);
-			}
-			System.Console.WriteLine(output);
-		}
-	}
+	// private void CompileProtoFiles(string protocPath, string inputPath, string outputPath)
+	// {
+	// 	var protoFiles = Directory.GetFiles(inputPath, "*.proto");
+	// 	foreach (var protoFile in protoFiles)
+	// 	{
+	// 		var process = new Process();
+	// 		process.StartInfo.FileName = protocPath;
+	// 		process.StartInfo.Arguments = $"--proto_path={inputPath} --cpp_out={outputPath} {protoFile}";
+	// 		process.StartInfo.UseShellExecute = false;
+	// 		process.StartInfo.RedirectStandardOutput = true;
+	// 		process.StartInfo.RedirectStandardError = true;
+	// 		process.Start();
+	// 		process.WaitForExit();
+	//
+	// 		string output = process.StandardOutput.ReadToEnd();
+	// 		string errors = process.StandardError.ReadToEnd();
+	// 		if (!string.IsNullOrEmpty(errors) || process.ExitCode != 0)
+	// 		{
+	// 			throw new Exception("Error compiling " + protoFile + ": " + errors);
+	// 		}
+	// 		System.Console.WriteLine(output);
+	// 	}
+	// }
 }
