0c33cc7690d49d580228981b84e4ca6f3ad23b4d
parkjoungwan <whddhs6645@gmail.com>
2024-06-13 10:32:10 +0900

    exception처리 FUdpFlush 헤더 추가

--- proj_a/Source/proj_a/ServerNetworkIO/Private/ListenSocket.cpp
+++ proj_a/Source/proj_a/ServerNetworkIO/Private/ListenSocket.cpp
@@ -4,6 +4,7 @@
 #include "SocketSubsystem.h"
 #include "Interfaces/IPv4/IPv4Address.h"
 #include "FUdpFlush.h"
+#include "FClientHandler.h"
 #include <exception>
 
 FListenSocketRunnable::FListenSocketRunnable(bool& bis_game_started)
