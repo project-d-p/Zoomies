859ec09bd1185b43483e7ecfd32917a3c3accf3a
parkjoungwan <whddhs6645@gmail.com>
2024-06-20 22:57:57 +0900

    Add Job & Animal & CalculateMathod

--- proj_a/Source/proj_a/Component/InGame/Score/CalculateScoreByJobs.h
+++ proj_a/Source/proj_a/Component/InGame/Score/CalculateScoreByJobs.h
@@ -14,10 +14,12 @@ public:
 	UCalculateScoreByJobs();
 	static FScoreData CalculateScoreByJobs(EPlayerJob Job, const TArray<EAnimal>& animals);
 private:
-	static FScoreData CalculateArchaeologist( const TArray<EAnimal>& animals);
-	static FScoreData CalculatePoacher( const TArray<EAnimal>& animals);
-	static FScoreData CalculateRingmaster( const TArray<EAnimal>& animals);
-	static FScoreData CalculateTerrorist( const TArray<EAnimal>& animals);
-	static FScoreData CalculateEnvironmentalist( const TArray<EAnimal>& animals);
+	static FScoreData CalScoreByJobs(
+		const TArray<EAnimal>& animals,
+		EAnimal favoriteAnimal,
+		const TArray<EAnimal>& addCombine,
+		const TArray<EAnimal>& mulCombine
+		);
 	static bool AnimalCombinationCheck ( const TArray<EAnimal>& animals, const TArray<EAnimal>& combination);
+	static int CalBaseScore (const TArray<EAnimal>& animals, const EAnimal& favoriteAnimal);
 };
