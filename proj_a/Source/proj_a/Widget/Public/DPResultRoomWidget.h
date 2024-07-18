505484e4728479543536c56bb180ac140ac0bee0
deulee <ideade9@gmail.com>
2024-07-18 14:24:58 +0900

    FIX: Gun Hit Effect

--- proj_a/Source/proj_a/Widget/Public/DPResultRoomWidget.h
+++ proj_a/Source/proj_a/Widget/Public/DPResultRoomWidget.h
@@ -14,6 +14,8 @@ class PROJ_A_API UDPResultRoomWidget : public UUserWidget
 {
 	GENERATED_BODY()
 public:
+	virtual void NativeConstruct() override;
+	
     UFUNCTION(BlueprintCallable, Category = "UI")
     TArray<int32> UpdateScores(const TArray<int32>& scores);
 };
