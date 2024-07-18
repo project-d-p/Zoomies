8993dd4721b4f998309275bd79a46e66f9f4ea96
deulee <ideade9@gmail.com>
2024-07-18 14:37:37 +0900

    Add: Result Score Variables in ResultLevelGameMode

--- proj_a/Source/proj_a/ResultLevel/GameMode/Public/ResultLevelGameMode.h
+++ proj_a/Source/proj_a/ResultLevel/GameMode/Public/ResultLevelGameMode.h
@@ -4,11 +4,29 @@
 #include "GameFramework/GameModeBase.h"
 #include "ResultLevelGameMode.generated.h"
 
+USTRUCT(BlueprintType)
+struct FPlayerScore
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "PlayerName")
+	FString PlayerName;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "PlayerJob")
+	FString PlayerJob;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "PlayerScore")
+	TArray<int32> Scores;
+
+	UPROPERTY(BlueprintReadWrite, Category = "bIsDetected")
+	bool bIsDetected;
+};
+
 UCLASS()
 class PROJ_A_API AResultLevelGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
 public:
 	AResultLevelGameMode();
 	virtual ~AResultLevelGameMode() override;
@@ -17,6 +35,9 @@ public:
 	virtual void HandleSeamlessTravelPlayer(AController*& C) override;
 	/* Seamless Travel : Reuse PlayerController for Server */
 	virtual void PostSeamlessTravel() override;
+
+	UPROPERTY(BlueprintReadWrite, Category = "PlayerScores")
+	TArray<FPlayerScore> PlayerScores;
 	
 protected:
 	virtual void Logout(AController* Exiting) override;
--- proj_a/Source/proj_a/ResultLevel/GameState/Public/ResultLevelGameState.h
+++ proj_a/Source/proj_a/ResultLevel/GameState/Public/ResultLevelGameState.h
@@ -10,9 +10,8 @@ class AResultLevelGameState : public AGameStateBase
 	GENERATED_BODY()
 public:
 	AResultLevelGameState();
-
+	
 protected:
 	virtual void AddPlayerState(APlayerState* PlayerState) override;
 	virtual void BeginPlay() override;
-	
 };
