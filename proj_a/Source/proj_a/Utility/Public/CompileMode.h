ae5f569fc04d5342bc59a218e0126a47d7d32133
deulee <ideade9@gmail.com>
2024-08-28 19:42:30 +0900

    Temporary Save for Test Online

--- proj_a/Source/proj_a/Utility/Public/CompileMode.h
+++ proj_a/Source/proj_a/Utility/Public/CompileMode.h
@@ -8,7 +8,7 @@
 //     1       |    0     : Editor
 
 #ifndef EDITOR_MODE
-	#define EDITOR_MODE 1
+	#define EDITOR_MODE 0
 #endif
 
 #ifndef LAN_MODE
@@ -17,7 +17,7 @@
 
 namespace Zoomies
 {
-	constexpr int MAX_MONSTERS = 100;
-	constexpr int MAX_PLAYERS = 1;
-	constexpr float GAME_TIME = 10.f;
+	constexpr int MAX_MONSTERS = 20;
+	constexpr int MAX_PLAYERS = 2;
+	constexpr float GAME_TIME = 40.f;
 }
