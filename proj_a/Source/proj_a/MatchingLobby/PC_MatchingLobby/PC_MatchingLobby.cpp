14652ec59f242701606a24fac5b0a00acd80fd0e
parkjoungwan <whddhs6645@gmail.com>
2024-08-28 10:11:03 +0900

    rollback pc & rm screen logging

--- proj_a/Source/proj_a/MatchingLobby/PC_MatchingLobby/PC_MatchingLobby.cpp
+++ proj_a/Source/proj_a/MatchingLobby/PC_MatchingLobby/PC_MatchingLobby.cpp
@@ -1,105 +1,12 @@
 #include "PC_MatchingLobby.h"
-#include "GameFramework/Character.h"
-#include "Net/UnrealNetwork.h"
+
 #include "CineCameraActor.h"
-#include "Kismet/GameplayStatics.h"
 #include "OnlineSessionSettings.h"
+#include "GameFramework/PlayerState.h"
+#include "Kismet/GameplayStatics.h"
 #include "proj_a/MatchingLobby/GM_MatchingLobby/GM_MatchingLobby.h"
 #include "proj_a/MatchingLobby/GS_MachingLobby/GS_MatchingLobby.h"
-
-APC_MatchingLobby::APC_MatchingLobby()
-{
-	bReplicates = true; // 클래스 자체가 복제 가능하도록 설정
-}
-
-void APC_MatchingLobby::BeginPlay()
-{
-	Super::BeginPlay();
-
-	ControlledPawn = GetPawn();
-
-	bShowMouseCursor = true;
-	FInputModeUIOnly InputMode;
-	SetInputMode(InputMode);
-
-}
-
-void APC_MatchingLobby::SetupInputComponent()
-{
-	Super::SetupInputComponent();
-
-	// 입력 바인딩
-	InputComponent->BindAxis("MoveForward", this, &APC_MatchingLobby::MoveForward);
-	InputComponent->BindAxis("MoveRight", this, &APC_MatchingLobby::MoveRight);
-}
-
-void APC_MatchingLobby::MoveForward(float Value)
-{
-	if (Value != 0.0f && ControlledPawn)
-	{
-		// 디버그 메시지 출력
-		if (GEngine)
-		{
-			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("MoveForward: %f"), Value));
-		}
-
-		// 서버에 입력값 전달
-		if (IsLocalController())
-		{
-			ServerMoveForward(Value);
-		}
-
-		// 서버라면 직접 이동 처리
-		if (GetLocalRole() == ROLE_Authority)
-		{
-			ControlledPawn->AddMovementInput(FVector::ForwardVector, Value);
-		}
-	}
-}
-
-void APC_MatchingLobby::MoveRight(float Value)
-{
-	if (Value != 0.0f && ControlledPawn)
-	{
-		// 디버그 메시지 출력
-		if (GEngine)
-		{
-			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("MoveRight: %f"), Value));
-		}
-
-		// 서버에 입력값 전달
-		if (IsLocalController())
-		{
-			ServerMoveRight(Value);
-		}
-
-		// 서버라면 직접 이동 처리
-		if (GetLocalRole() == ROLE_Authority)
-		{
-			ControlledPawn->AddMovementInput(FVector::RightVector, Value);
-		}
-	}
-}
-
-void APC_MatchingLobby::ServerMoveForward_Implementation(float Value)
-{
-	MoveForward(Value);
-}
-
-bool APC_MatchingLobby::ServerMoveForward_Validate(float Value)
-{
-	return true;
-}
-
-void APC_MatchingLobby::ServerMoveRight_Implementation(float Value)
-{
-	MoveRight(Value);
-}
-
-bool APC_MatchingLobby::ServerMoveRight_Validate(float Value)
-{
-	return true;
-}
+#include "Interfaces/OnlineSessionInterface.h"
 
 void APC_MatchingLobby::ServerSetReady_Implementation(bool pIsReady)
 {
@@ -124,6 +31,21 @@ void APC_MatchingLobby::ToggleReadyState()
 	ServerSetReady(bIsReady);
 }
 
+bool APC_MatchingLobby::GetIsReady()
+{
+	return bIsReady;
+}
+
+void APC_MatchingLobby::BeginPlay()
+{
+	Super::BeginPlay();
+
+	bShowMouseCursor = true;
+	
+	FInputModeUIOnly InputMode;
+	SetInputMode(InputMode);
+}
+
 void APC_MatchingLobby::SetCineCameraView()
 {
 	TArray<AActor*> FoundActors;
@@ -146,11 +68,4 @@ void APC_MatchingLobby::SetCineCameraView()
 	}
 
 	this->SetViewTargetWithBlend(CineCamera);
-}
-
-void APC_MatchingLobby::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-
-	DOREPLIFETIME(APC_MatchingLobby, bIsReady);
 }
--- proj_a/Source/proj_a/MatchingLobby/PC_MatchingLobby/PC_MatchingLobby.h
+++ proj_a/Source/proj_a/MatchingLobby/PC_MatchingLobby/PC_MatchingLobby.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "proj_a/MatchingLobby/TYPE_MatchingLobby/TYPE_MatchingLobby.h"
 #include "PC_MatchingLobby.generated.h"
 
 UCLASS()
@@ -10,42 +11,21 @@ class PROJ_A_API APC_MatchingLobby : public APlayerController
 	GENERATED_BODY()
 
 public:
-	APC_MatchingLobby();
-
-protected:
-	virtual void BeginPlay() override;
-	virtual void SetupInputComponent() override;
-	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
-
-public:
-	// 이동 관련 함수
-	UFUNCTION(Server, Reliable, WithValidation)
-	void ServerMoveForward(float Value);
-
-	UFUNCTION(Server, Reliable, WithValidation)
-	void ServerMoveRight(float Value);
-
-	UFUNCTION()
-	void MoveForward(float Value);
-
-	UFUNCTION()
-	void MoveRight(float Value);
-
-	// 준비 상태 관련 함수
 	UFUNCTION(Server, Reliable, WithValidation)
 	void ServerSetReady(bool pIsReady);
 
 	UFUNCTION(BlueprintCallable, Category = "MatchingLobby")
 	void ToggleReadyState();
-
 	void SetCineCameraView();
 
 	UPROPERTY(BlueprintReadWrite, Category = "MatchingLobbyInfo")
 	bool bIsReady = false;
-
 	UPROPERTY(BlueprintReadWrite, Category = "MatchingLobbyInfo")
 	FString UserName = "DefaultName";
+	UFUNCTION(BlueprintCallable, Category = "Steam")
 
+	bool GetIsReady();
+protected:
+	virtual void BeginPlay() override;
 private:
-	APawn* ControlledPawn;
 };
