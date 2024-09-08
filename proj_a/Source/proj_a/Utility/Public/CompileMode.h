4fd042685c7ff05fab714ea093b650a42c2ba087
deulee <ideade9@gmail.com>
2024-09-08 18:04:58 +0900

    TEST Play

--- proj_a/Source/proj_a/Utility/Public/CompileMode.h
+++ proj_a/Source/proj_a/Utility/Public/CompileMode.h
@@ -8,7 +8,7 @@
 //     1       |    0     : Editor
 
 #ifndef EDITOR_MODE
-	#define EDITOR_MODE 1
+	#define EDITOR_MODE 0
 #endif
 
 #ifndef LAN_MODE
@@ -18,6 +18,6 @@
 namespace Zoomies
 {
 	constexpr int MAX_MONSTERS = 20;
-	constexpr int MAX_PLAYERS = 2;
-	constexpr float GAME_TIME = 40.f;
+	constexpr int MAX_PLAYERS = 4;
+	constexpr float GAME_TIME = 300.f;
 }
