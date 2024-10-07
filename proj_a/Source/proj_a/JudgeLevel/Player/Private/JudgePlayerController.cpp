9b220fb57b2fe540cd08c154757cd1abdcc9d739
deulee <ideade9@gmail.com>
2024-10-07 17:15:21 +0900

    Feat: Fix Data Tranfer Between Level Transmition

--- proj_a/Source/proj_a/JudgeLevel/Player/Private/JudgePlayerController.cpp
+++ proj_a/Source/proj_a/JudgeLevel/Player/Private/JudgePlayerController.cpp
@@ -118,13 +118,6 @@ void AJudgePlayerController::SeamlessTravelFrom(APlayerController* OldPC)
 	AJudgePlayerState* GS = GetPlayerState<AJudgePlayerState>();
 	checkf(GS, TEXT("Failed to get JudgePlayerState"))
 	checkf(MainPC->GetPrivateScoreManagerComponent(), TEXT("Failed to get PrivateScoreManagerComponent"))
-	ADPPlayerState* OPS = MainPC->GetPlayerState<ADPPlayerState>();
-	checkf(OPS, TEXT("Failed to get DPPlayerState"))
-	GS->SetPlayerScoreData(OPS->GetPlayerScoreData());
-	// GS->SetScore(MainPC->GetPrivateScoreManagerComponent()->GetPrivatePlayerScore());
-	//
-	// GS->SetCapturedAnimals(MainPC->GetPrivateScoreManagerComponent()->GetCapturedAnimals());
-	// GS->SetScoreDatas(MainPC->GetPrivateScoreManagerComponent()->GetScoreDatas());
 }
 
 void AJudgePlayerController::SeamlessTravelTo(APlayerController* NewPC)
@@ -137,6 +130,5 @@ void AJudgePlayerController::SeamlessTravelTo(APlayerController* NewPC)
 	check(NGS)
 	AJudgePlayerState* GS = GetPlayerState<AJudgePlayerState>();
 	check(GS)
-	// NGS->SetFinalScoreData(GS->GetFinalScoreData());
 	NGS->SetPlayerScoreData(GS->GetPlayerScoreData());
 }
