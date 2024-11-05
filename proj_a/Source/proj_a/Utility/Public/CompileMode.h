9c5a28406308af73644018b4975cdd8828f88c42
deulee <ideade9@gmail.com>
2024-11-05 19:00:30 +0900

    FIX: Session Problem (Done)

--- proj_a/Source/proj_a/Utility/Public/CompileMode.h
+++ proj_a/Source/proj_a/Utility/Public/CompileMode.h
@@ -18,7 +18,7 @@
 namespace Zoomies	
 {
 	constexpr int MAX_MONSTERS = 15;
-	constexpr int MAX_PLAYERS = 2;
+	constexpr int MAX_PLAYERS = 4;
 	constexpr float GAME_TIME = 50.f;
-	constexpr float JUDGE_TIME = 30.f;
+	constexpr float JUDGE_TIME = 20.f;
 }
