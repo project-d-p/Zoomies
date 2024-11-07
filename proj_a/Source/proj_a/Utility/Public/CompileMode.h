ab5c6e4f7de812d992223fef3ee97e9bdca5b691
deulee <ideade9@gmail.com>
2024-11-07 20:42:24 +0900

    FEat: Logic For Decreasing Clients in Judge Level

--- proj_a/Source/proj_a/Utility/Public/CompileMode.h
+++ proj_a/Source/proj_a/Utility/Public/CompileMode.h
@@ -12,13 +12,13 @@
 #endif
 
 #ifndef LAN_MODE
-	#define LAN_MODE 0
+	#define LAN_MODE 1
 #endif
 
 namespace Zoomies	
 {
 	constexpr int MAX_MONSTERS = 15;
-	constexpr int MAX_PLAYERS = 1;
+	constexpr int MAX_PLAYERS = 3;
 	constexpr float GAME_TIME = 20.f;
 	constexpr float JUDGE_TIME = 20.f;
 }
