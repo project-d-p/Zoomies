c23ca8a390dfd8c62090d773200a9ce27b72c985
deulee <ideade9@gmail.com>
2024-11-12 19:09:16 +0900

    TEST

--- proj_a/Source/proj_a/Actor/Controller/PlayerController/Components/InputComponents/Private/BasicInputComponent.cpp
+++ proj_a/Source/proj_a/Actor/Controller/PlayerController/Components/InputComponents/Private/BasicInputComponent.cpp
@@ -67,6 +67,7 @@ void UBasicInputComponent::BindBasicLevelActions()
 		EnhancedInputComponent->BindAction(RotateAction, ETriggerEvent::Triggered, this, &UBasicInputComponent::Rotate);
 		// 달리기 ( shift )
 		EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Triggered, this, &UBasicInputComponent::Run);
+		FNetLogger::LogError(TEXT("BindBasicLevelActions: %s, %s"), *PlayerController->PlayerState->GetName(), GetWorld()->GetMapName());
 	}
 }
 
