bbadcb34e9c035b68049ac4e7aa01aab499f5427
parkjoungwan <whddhs6645@gmail.com>
2024-11-13 11:06:52 +0900

    Check Animal_Combination

--- proj_a/Source/proj_a/Component/InGame/Score/CalculateScoreByJobs.cpp
+++ proj_a/Source/proj_a/Component/InGame/Score/CalculateScoreByJobs.cpp
@@ -31,8 +31,8 @@ FScoreData UCalculateScoreByJobs::CalculateScoreByJobs(EPlayerJob Job, const TAr
 					EAnimal::ANIMAL_GIRAFFE,
 					{TArray<EAnimal>{
 						EAnimal::ANIMAL_GIRAFFE,
-						// EAnimal::ANIMAL_RABBIT,
-						// EAnimal::ANIMAL_SHEPHERD
+						EAnimal::ANIMAL_CHAMELEON,
+						EAnimal::ANIMAL_HORSE
 						}},
 					{TArray<EAnimal>{
 						EAnimal::ANIMAL_SABER_TOOTH_TIGER,
@@ -53,7 +53,7 @@ FScoreData UCalculateScoreByJobs::CalculateScoreByJobs(EPlayerJob Job, const TAr
 					EAnimal::ANIMAL_LION,
 					EAnimal::ANIMAL_SABER_TOOTH_TIGER,
 					EAnimal::ANIMAL_SEAL,
-					// EAnimal::ANIMAL_RABBIT,
+					EAnimal::ANIMAL_CHAMELEON,
 					EAnimal::ANIMAL_FOX}});
 			break;
 		case EPlayerJob::JOB_TERRORIST:
