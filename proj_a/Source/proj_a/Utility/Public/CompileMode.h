23fc80483dd5710a54533eeff44bebc2f913cd55
deulee <ideade9@gmail.com>
2024-11-13 17:30:18 +0900

    TEST

--- proj_a/Source/proj_a/Utility/Public/CompileMode.h
+++ proj_a/Source/proj_a/Utility/Public/CompileMode.h
@@ -19,6 +19,6 @@ namespace Zoomies
 {
 	constexpr int MAX_MONSTERS = 15;
 	constexpr int MAX_PLAYERS = 3;
-	constexpr float GAME_TIME = 100.f;
+	constexpr float GAME_TIME = 60.f;
 	constexpr float JUDGE_TIME = 30.f;
 }
